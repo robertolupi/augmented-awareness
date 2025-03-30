730279e6da18b96f1d7d353b0e33fb81969fb50c
Roberto Lupi <roberto.lupi@gmail.com>
2025-03-30 15:10:56 +0200

    Python uv project + base handling of Obsidian vault.

---
+++ python/aww/observe/obsidian.py
@@ -0,0 +1,95 @@
+"""Observe the content of an Obsidian vault."""
+
+from typing import TypeVar
+
+
+import collections
+import os
+import datetime
+import pathlib
+import re
+
+import rich
+import rich.markdown
+import yaml
+
+import mistune
+
+class Vault:
+    """An Obsidian vault."""
+
+    path: pathlib.Path
+
+    def __init__(self, path: pathlib.Path | str):
+        self.path = pathlib.Path(path)
+        if not self.path.is_dir():
+            raise ValueError(f"Path {self.path} is not a directory.")
+        if not (self.path / ".obsidian").is_dir():
+            raise ValueError(f"Path {self.path} is not an Obsidian vault.")
+
+    def pages(self) -> dict[str, 'Page']:
+        """Get the pages in the vault."""
+        pages = {}
+        for root, _, files in os.walk(self.path):
+            for file in files:
+                if file.endswith(".md"):
+                    page_path = pathlib.Path(root) / file
+                    page = Page(page_path)
+                    pages[page.name] = page
+        return pages
+
+    def journal(self) -> dict[datetime.datetime, 'Page']:
+        """Get the pages corresponding to dates in the vault."""
+        DATE_RE = re.compile(r"^\d{4}-\d{2}-\d{2}$")
+        pages = []
+        for pagename, page in self.pages().items():
+            if DATE_RE.match(pagename):
+                date = datetime.datetime.strptime(pagename, "%Y-%m-%d").date()
+                pages.append((date, page))
+        pages.sort(key=lambda x: x[0])
+        return collections.OrderedDict(pages)
+
+
+class Markdown(str):
+    """A Markdown string."""
+
+    def __rich__(self) -> rich.console.RenderableType:
+        return rich.markdown.Markdown(self)
+
+    def parse(self) -> list[dict]:
+        from mistune.plugins.task_lists import task_lists
+        md = mistune.Markdown(renderer=None, plugins=[task_lists])
+        return md(self)
+
+
+class Page:
+    """An Obsidian page."""
+
+    path: pathlib.Path
+
+    def __init__(self, path: pathlib.Path):
+        self.path = path
+
+    @property
+    def name(self) -> str:
+        """Get the name of the page."""
+        return self.path.stem
+
+    @property
+    def frontmatter(self) -> dict:
+        """Get the frontmatter of the page."""
+        with open(self.path, "r", encoding="utf-8") as f:
+            content = f.read()
+        if content.startswith("---\n"):
+            _, frontmatter, _ = content.split("---\n", 2)
+            return yaml.safe_load(frontmatter)
+        return {}
+
+    @property
+    def content(self) -> Markdown:
+        """Get the content of the page."""
+        with open(self.path, "r", encoding="utf-8") as f:
+            content = f.read()
+        if content.startswith("---"):
+            _, _, content = content.split("---\n", 2)
+        return Markdown(content)
---
+++ python/test/test_obsidian.py
@@ -0,0 +1,74 @@
+import pytest
+from aww.observe.obsidian import Vault
+import pathlib
+import datetime
+import rich
+import rich.markdown
+
+test_dir = pathlib.Path(__file__).parent
+test_vault_dir = test_dir / "vault"
+
+
+def test_invalid_vault():
+    """Test that an invalid vault raises a ValueError."""
+    with pytest.raises(ValueError):
+        Vault("invalid_path")
+    with pytest.raises(ValueError):
+        Vault(".")
+
+
+def test_valid_vault():
+    v = Vault(test_vault_dir)
+    assert v.path.name == "vault"
+
+
+def test_pages():
+    v = Vault(test_vault_dir)
+    pages = v.pages()
+    assert len(pages) == 2
+
+
+def test_journal():
+    v = Vault(test_vault_dir)
+    journal = v.journal()
+    assert len(journal) == 1
+    assert datetime.date(2025, 3, 30) in journal
+
+
+def test_frontmatter():
+    v = Vault(test_vault_dir)
+    page = v.pages()["2025-03-30"]
+    assert page.frontmatter == {"stress": 4}
+
+
+def test_markdown():
+    v = Vault(test_vault_dir)
+    page = v.pages()["index"]
+    assert page.content == "# Obsidian Test Vault\n\nJust a simple page.\n\n---\n\nThis page has no frontmatter.\n"
+    assert isinstance(page.content.__rich__(), rich.markdown.Markdown)
+
+
+def test_markdown_parse():
+    v = Vault(test_vault_dir)
+    page = v.pages()["index"]
+    parsed = page.content.parse()
+    assert parsed == [
+        {
+            'type': 'heading',
+            'attrs': {'level': 1},
+            'style': 'atx',
+            'children': [{'type': 'text', 'raw': 'Obsidian Test Vault'}]
+        },
+        {'type': 'blank_line'},
+        {
+            'type': 'paragraph',
+            'children': [{'type': 'text', 'raw': 'Just a simple page.'}]
+        },
+        {'type': 'blank_line'},
+        {'type': 'thematic_break'},
+        {'type': 'blank_line'},
+        {
+            'type': 'paragraph',
+            'children': [{'type': 'text', 'raw': 'This page has no frontmatter.'}]
+        }
+    ]
