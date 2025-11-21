ae14ddf9915d306538c2e6a6acd76d953e583158
Roberto Lupi <roberto.lupi@gmail.com>
2025-11-21 09:08:32 +0100

    chat: include memories
    Fetch the content of aww-scratchpad and include it in the system prompt when chatting.

--- aww/cli/chat.py
+++ aww/cli/chat.py
@@ -8,6 +8,7 @@ from pydantic_ai.mcp import MCPServerStdio, CallToolFunc, ToolResult
 from aww.cli import main
 from aww.chat import get_chat_agent
 from aww.prompts import select_prompt_template
+from aww.obsidian import Page
 
 
 @main.command(name="chat")
@@ -18,8 +19,16 @@ def chat(ctx):
     llm_model = ctx.obj["llm_model"]
     agent = get_chat_agent(llm_model)
 
+    scratchpad: Page | None = None
+    for page in vault.walk():
+        if page.name == 'aww-scratchpad':
+            scratchpad = page
+
     @agent.system_prompt
     def system_prompt():
-        return select_prompt_template(["chat.md"]).render()
+        result = select_prompt_template(["chat.md"]).render()
+        if scratchpad:
+            result += "The content of your memories in the [[aww-scratchpad]] page is:\n" + scratchpad.content()
+        return result
 
     agent.to_cli_sync(prog_name="aww")
