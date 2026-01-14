6434e440c424137e652685bc0668694f95c53856
Roberto Lupi <roberto.lupi@gmail.com>
2026-01-14 12:04:30 +0100

    Introduce new /activities endpoint in "journal pomodoro"

--- cmd/pomodoro.go
+++ cmd/pomodoro.go
@@ -20,6 +20,37 @@ const (
 	breakToIdle = "break_to_idle"
 )
 
+type Activity struct {
+	Description string `json:"description"`
+	StartTime   int64  `json:"startTime"` // Milliseconds
+	EndTime     *int64 `json:"endTime"`   // Milliseconds
+	Tags        string `json:"tags"`
+}
+
+func (a *Activity) ToDateAndEvent() (datetime.Date, *obsidian.Event) {
+	var startTime = time.Unix(0, a.StartTime*int64(time.Millisecond))
+	var endTime time.Time
+	if a.EndTime != nil {
+		endTime = time.Unix(0, *a.EndTime*int64(time.Millisecond))
+	}
+	var endtime2 datetime.Time = datetime.EmptyTime
+	if !endTime.IsZero() {
+		endtime2 = datetime.Time(endTime.Format("15:04"))
+	}
+
+	tags := strings.Split(a.Tags, ",")
+	for i, tag := range tags {
+		tags[i] = "#" + tag
+	}
+
+	return datetime.Date(startTime.Format("2006-01-02")), &obsidian.Event{
+		StartTime: datetime.Time(startTime.Format("15:04")),
+		EndTime:   endtime2,
+		Text:      fmt.Sprintf("%s %s", a.Description, strings.Join(tags, " ")),
+		Tags:      tags,
+	}
+}
+
 type Pomodoro struct {
 	StartTime  uint64 `json:"start_time"`
 	EventTime  uint64 `json:"event_time"`
@@ -46,76 +77,111 @@ func (p *Pomodoro) ToDateAndEvent() (datetime.Date, *obsidian.Event) {
 	}
 }
 
+func saveEvent(date datetime.Date, event *obsidian.Event) error {
+	page, err := app.Vault.Page(date.String())
+	if err != nil {
+		return err
+	}
+
+	section, err := page.FindSection(journalSection)
+	if err != nil {
+		return err
+	}
+
+	if err := section.AddEvent(*event); err != nil {
+		return err
+	}
+
+	if err := page.Save(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func handlePomodoroTransition(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// extract transition id
+	id := strings.Split(r.URL.Path, "/")[2]
+	// Decode JSON body
+	var data Pomodoro
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	date, event := data.ToDateAndEvent()
+
+	fmt.Println("Received pomodoro transition for id:", id)
+	fmt.Printf("Date: %s\n", date)
+	fmt.Printf("Event: %+v\n", event)
+
+	if data.Transition == idleToWork || data.Transition == breakToIdle {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	err = saveEvent(date, event)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Error saving event:", err)
+		return
+	}
+}
+
+func handleActivities(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Decode JSON body
+	var data []Activity
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	for n, activity := range data {
+		date, event := activity.ToDateAndEvent()
+
+		fmt.Printf("Received Activity %d\n", n+1)
+		fmt.Printf("Date: %s\n", date)
+		fmt.Printf("Event: %+v\n", event)
+
+		err := saveEvent(date, event)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Println("Error saving event:", err)
+			return
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 var (
 	pomodoroPort string
 	pomodoroCmd  = &cobra.Command{
 		Use:   "pomodoro",
 		Short: "Receive pomodoro timer notifications",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// Start a HTTP server that listens on the specified port
-			// and receives notifications from the pomodoro timer.
-
-			// Handle POST requests to //pomodoros/{id}/transitions with JSON body
-			// containing the transition data.
-
-			// For now, we will just print the transition data to the console.
-			http.HandleFunc("/pomodoros/{id}/transitions", func(w http.ResponseWriter, r *http.Request) {
-				if r.Method != "POST" {
-					w.WriteHeader(http.StatusMethodNotAllowed)
-					return
-				}
-
-				// extract transition id
-				id := strings.Split(r.URL.Path, "/")[2]
-				// Decode JSON body
-				var data Pomodoro
-				err := json.NewDecoder(r.Body).Decode(&data)
-				if err != nil {
-					w.WriteHeader(http.StatusBadRequest)
-					fmt.Println("Error decoding JSON:", err)
-					return
-				}
-
-				date, event := data.ToDateAndEvent()
-
-				fmt.Println("Received pomodoro transition for id:", id)
-				fmt.Printf("Date: %s\n", date)
-				fmt.Printf("Event: %+v\n", event)
-
-				if data.Transition == idleToWork || data.Transition == breakToIdle {
-					w.WriteHeader(http.StatusOK)
-					return
-				}
-
-				page, err := app.Vault.Page(date.String())
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					fmt.Println("Error getting page:", err)
-					return
-				}
-
-				section, err := page.FindSection(journalSection)
-				if err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					fmt.Println("Error finding section:", err)
-					return
-				}
-
-				if err := section.AddEvent(*event); err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					fmt.Println("Error adding event:", err)
-					return
-				}
-
-				if err := page.Save(); err != nil {
-					w.WriteHeader(http.StatusInternalServerError)
-					fmt.Println("Error saving page:", err)
-					return
-				}
-			})
+			http.HandleFunc("/pomodoros/{id}/transitions", handlePomodoroTransition)
+			http.HandleFunc("/activities", handleActivities)
 
 			fmt.Println("Listening on port :" + pomodoroPort)
-			http.ListenAndServe(":"+pomodoroPort, nil)
+			err := http.ListenAndServe(":"+pomodoroPort, nil)
+			if err != nil {
+				fmt.Println("ListenAndServe error:", err)
+			}
 
 			return nil
 		},
